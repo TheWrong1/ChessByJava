cae8126a3ab23b0823383330aea0bd716fcd32be
SRG <sergey.hakobyan.prof@gmail.com>
2023-06-01 13:39:50 -0700

    V1.01

--- main.java
+++ main.java
@@ -2,6 +2,8 @@ public class main {
     public static void main(String[] args) {
 
         Position kaka = new Position(7, 0);
+        Position mama = new Position();
+        System.out.println(mama.toString());
         System.out.println(kaka.toString());
 
     }
