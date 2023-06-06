88436d7c3a0375f3a2aa6f0e6394516a675b22f1
SRG <sergey.hakobyan.prof@gmail.com>
2023-06-05 21:20:21 -0700

    V1.08 Fully Operational

--- chess/main.java
+++ chess/main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input '' -console '' to play using the console");
+        System.out.println("Input \"-console\" to play using the console");
         System.out.println("Anything else inputted, and you play with the GUI");
         if (sc.nextLine() == "-console") {
             ChessConsole myGame = new ChessConsole();
